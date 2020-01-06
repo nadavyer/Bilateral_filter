ff77643f8bc5a5cf5eb34a792d4e1b472141e2de
nadav <nadavyerushalmi@gmail.com>
2020-01-06 22:11:04 +0200

    working on alg

--- blf.py
+++ blf.py
@@ -18,31 +18,27 @@ def gaussian(x, sigma):
     return (1.0 / (2 * math.pi * (sigma ** 2))) * math.exp(- (x ** 2) / (2 * sigma ** 2))
 
 
-def apply_bilateral_filter(source, filtered_image, x, y, diameter, sigma_i, sigma_s):
+def apply_bilateral_filter(source, filtered_image, row, col, diameter, sigma_i, sigma_s):
     hl = int(diameter/2)
     i_filtered = 0
     Wp = 0
     i = 0
     while i < diameter:
         j = 0
-        neighbour_x = int(x - (hl - i))
-        if neighbour_x >= len(source) or neighbour_x < 0:
-            i += 1
-            continue
-        while j < diameter:
-            neighbour_y = int(y - (hl - j))
-            if neighbour_y >= len(source) or neighbour_y < 0:
+        neighbour_row = int(row - (hl - i))
+        if len(source) > neighbour_row >= 0:
+            while j < diameter:
+                neighbour_col = int(col - (hl - j))
+                if len(source) > neighbour_col >= 0:
+                    gi = gaussian(source[neighbour_row][neighbour_col] - source[row][col], sigma_i)
+                    gs = gaussian(distance(neighbour_row, neighbour_col, row, col), sigma_s)
+                    w = gi * gs
+                    i_filtered += source[neighbour_row][neighbour_col] * w
+                    Wp += w
                 j += 1
-                continue
-            gi = gaussian(source[neighbour_x][neighbour_y] - source[x][y], sigma_i)
-            gs = gaussian(distance(neighbour_x, neighbour_y, x, y), sigma_s)
-            w = gi * gs
-            i_filtered += source[neighbour_x][neighbour_y] * w
-            Wp += w
-            j += 1
         i += 1
     i_filtered = i_filtered / Wp
-    filtered_image[x][y] = i_filtered
+    filtered_image[row][col] = i_filtered
 
 
 def bilateral_filter_own(args):
@@ -60,10 +56,12 @@ def bilateral_filter_own(args):
 
 
 if __name__ == "__main__":
+
+    pixels = [[(10, 20, 30), (40, 50, 60), (70, 80, 90), (100, 110, 120),  (130, 140, 150), (200, 200, 200)]]
+    # cv2.imwrite("pixelim.jpg", pixels)
+    # arr = np.array(pixels, dtype=np.uint8)
     rb256 = cv2.imread(str(sys.argv[1]))
     blue, green, red = cv2.split(rb256)
-    merged = cv2.merge((blue, green, red))
-    cv2.imwrite("merged.jpg", merged)
     pool = Pool(processes=PROC_NUM)
     future_blue, future_green, future_red = pool.map(bilateral_filter_own, [(blue, FILTER_DIAMETER, SIGMA_I, SIGMA_S),
                                                                             (green, FILTER_DIAMETER, SIGMA_I, SIGMA_S),
